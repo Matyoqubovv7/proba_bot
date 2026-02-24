4051bbed17930a4d044b47c92914d32c0edac0f8
Khudashkur <mxudashkur17@gmail.>
2026-02-24 17:56:45 +0500

    mybots version 1.1

--- handlers/users/echo.py
+++ handlers/users/echo.py
@@ -2,7 +2,6 @@ from aiogram import Router, F
 from aiogram.types import Message
 
 router = Router()
-router.message.filter(F.chat.type == "private")
 
 @router.message()
 async def bot_echo(message: Message):
